532826f59b46d6169e53c4c611d2484c1b66cea8
Danush <psdanush6900@gmail.com>
2022-03-09 10:53:56 +0530

    Valid Sudoku.java Added and Done

---
+++ Checker.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    a=b=c=d=10;
+    printf("%d %d %d %d",a,b,c,d);
+}
--- validSudoku.java
+++ validSudoku.java
@@ -1,5 +1,26 @@
 import java.util.*;
 public class validSudoku{
-    Scanner scanner = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] Rows = new int[9], Cols = new int[9], Matrix = new int[9];
+        int current;
+        for(int row=0; row<9; row++){
+            for(int col=0; col<9; col++){
+                current = scanner.nextInt();
+                Rows[row]|=1<<(current-1);
+                Cols[col]|=1<<(current-1);
+                Matrix[((row/3)*3)+(col/3)]|=1<<(current-1);
+            }
+        }
+        scanner.close();
+        int check = (1<<9)-1;
+        for(int i=0;i<9;i++){
+            // System.out.println(Rows[i]+" "+Cols[i]+" "+Matrix[i]);
+            if(Rows[i]!=check || Cols[i]!=check || Matrix[i]!=check){
+                System.out.println("Not Valid");
+                return;
+            }
+        }
+        System.out.println("Valid");
+    }
 }
