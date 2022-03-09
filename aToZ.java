33a93200d726e14595bdba2a289ae164c4c86490
Danush <psdanush6900@gmail.com>
2022-03-09 12:25:39 +0530

    Added AtoZjavaProgram

---
+++ aToZ.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class aToZ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] myArr = scanner.nextLine().toCharArray();
+        int result=0,len=myArr.length;
+        for(int i=0;i<len;i++){
+            result|=(1<<(myArr[i]-97));
+        }
+        System.out.println(result);
+        if(result==(1<<26)-1){
+            System.out.print("Yes");
+        }
+        else{
+            System.out.print("No");
+        }
+        scanner.close();
+    }
+}
