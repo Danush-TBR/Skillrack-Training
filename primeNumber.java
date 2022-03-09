0fbc68265895e41439dae2b768ec5f93c1f18033
Danush <psdanush6900@gmail.com>
2022-03-09 14:23:45 +0530

    Modified Prime

--- primeNumber.java
+++ primeNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.lang.Math;
+// import java.lang.Math;
 public class primeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
