44a4275c9185ded376be0b32864b94827063ed26
Danush <psdanush6900@gmail.com>
2022-03-10 10:44:26 +0530

    Added some code for Matrix DP

--- Dynamic Programming/MaximumCouponPathMatrix.java
+++ Dynamic Programming/MaximumCouponPathMatrix.java
@@ -9,25 +9,43 @@ public class MaximumCouponPathMatrix {
         int[][] arr=new int[R][C];
 
         //Getting Input Matrix
-        for(int i=0;i<R;i++){
-            for(int j=0;j<C;j++){
-                arr[i][j]=scanner.nextInt();
+        for(int row=0;row<R;row++){
+            for(int col=0;col<C;col++){
+                arr[row][col]=scanner.nextInt();
             }
         }
-        int[][] DP = new int[R+1][C+1];
-        for(int i=0;i<R;i++){
-            for(int j=0;j<C;j++){
-                DP[i+1][j+1]=Math.max(DP[i][j+1],DP[i+1][j])+arr[i][j];
+        for(int row=1;row<R;row++) arr[row][0]+=arr[row-1][0];
+        for(int col=1;col<C;col++) arr[0][col]+=arr[0][col-1];
+        for(int row=1;row<R;row++){
+            for(int col=1;col<C;col++){
+                arr[row][col]=Math.max(arr[row-1][col],arr[row][col-1])+arr[row][col];
             }
         }
         System.out.println();
-        for(int i=1;i<=R;i++){
-            for(int j=1;j<=C;j++){
-                System.out.print(DP[i][j]+" ");
+        for(int row=0;row<R;row++){
+            for(int col=0;col<C;col++){
+                System.out.print(arr[row][col]+" ");
             }
             System.out.println();
         }
+        System.out.print(arr[R-1][C-1]);
+        /* 
+        Using a secondary DP Matrix
+        int[][] DP = new int[R+1][C+1];
+        for(int row=0;row<R;row++){
+            for(int col=0;col<C;col++){
+                DP[row+1][col+1]=Math.max(DP[row][col+1],DP[row+1][col])+arr[row][col];
+            }
+        }
+        System.out.println();
+        for(int row=1;row<=R;row++){
+            for(int col=1;col<=C;col++){
+                System.out.print(DP[row][col]+" ");
+            }
+            System.out.println();
+        } 
         System.out.print(DP[R][C]);
+        */
         scanner.close();
     }
 }
