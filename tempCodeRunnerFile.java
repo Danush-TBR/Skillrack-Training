f96c92ff90cfa8b2e40b7f0f1d941e29d7a2b322
Danush <psdanush6900@gmail.com>
2022-03-10 10:53:19 +0530

    Deleted tempRunner file

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-// long mySqrt = (long)Math.sqrt(n);
-        // for(long i=2;i*i<=n;i++){
-        //     if(n%i==0){
-        //         System.out.println("Not a Prime Number");
-        //         return;
-        //     }
-        // }
-        // System.out.println("Prime Number");
