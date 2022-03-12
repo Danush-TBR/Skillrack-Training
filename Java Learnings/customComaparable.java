b4d42d2bec90139c7fc4243b9549eebf1e5ee777
Danush <psdanush6900@gmail.com>
2022-03-12 09:27:44 +0530

    Added Custom Comparable Java

---
+++ Java Learnings/customComaparable.java
@@ -0,0 +1,13 @@
+class Player implements Comparable<Player>{
+
+    @Override
+    public int compareTo(Player o) {
+        
+        return 0;
+    }
+}
+public class customComaparable {
+    public static void main(String[] args) {
+
+    }
+}
