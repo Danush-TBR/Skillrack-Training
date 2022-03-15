41284b61a27f8f1466ddb1b245a512b34a6cdd08
Danush <psdanush6900@gmail.com>
2022-03-15 12:15:51 +0530

    Java Learn

---
+++ Java Learnings/abc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class abc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // char ch; int n;
+        // ch=scanner.next().charAt(0);
+        // n=scanner.nextInt();
+        // System.out.println(ch+" "+n);
+        String str="Hello";
+        System.out.println(String.format("%d", 20));
+        scanner.close();
+    }
+}
