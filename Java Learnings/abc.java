ff4e094987bdbb809340f25e6bdc56301237cb55
Danush <psdanush6900@gmail.com>
2022-03-20 16:25:04 +0530

    Modified Abc.java

--- Java Learnings/abc.java
+++ Java Learnings/abc.java
@@ -7,8 +7,14 @@ public class abc {
         // ch=scanner.next().charAt(0);
         // n=scanner.nextInt();
         // System.out.println(ch+" "+n);
-        String str="Hello";
+        // String str="Hello";
         System.out.println(String.format("%d", 20));
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(0);
+        list.remove(0);
+        System.out.print(list);
         scanner.close();
     }
 }
